ace979d052b7c420005a5de022aaaf6565dbc307
SebastianLorenzano <selorenzano3@gmail.com>
2023-09-16 13:13:04 +0200

    second commit

---
+++ 0_exercise1/Form1.cs
@@ -0,0 +1,20 @@
+namespace _0_exercise1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("The First button has been pressed");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("The Second button has been pressed");
+        }
+    }
+}
