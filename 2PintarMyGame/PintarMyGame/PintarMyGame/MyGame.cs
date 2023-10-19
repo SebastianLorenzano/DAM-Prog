1da4d7cc04ef5c495c808b425e268e2ea31452b8
SebastianLorenzano <selorenzano3@gmail.com>
2023-10-19 18:43:05 +0200

    19-10-20203

--- 2PintarMyGame/PintarMyGame/PintarMyGame/MyGame.cs
+++ 2PintarMyGame/PintarMyGame/PintarMyGame/MyGame.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UDK;
 
 namespace PintarMyGame
 {
-    internal class MyGame
+    internal class MyGame : UDK.IGameDelegate
     {
+
+        public void OnLoad(GameDelegateEvent gameEvent)
+        {
+
+        }
+
+        public void OnKeyboard(GameDelegateEvent gameEvent, IKeyboard keyboard, IMouse mouse)
+        {
+
+        }
+
+        public void OnAnimate(GameDelegateEvent gameEvent)
+        {
+
+        }
+
+        public void OnDraw(GameDelegateEvent gameEvent, ICanvas canvas)
+        {
+
+        }
+
+        public void OnUnload(GameDelegateEvent gameEvent)
+        {
+
+        }
     }
 }
--- 2PintarMyGame/PintarMyGame/PintarMyGame/World.cs
+++ 2PintarMyGame/PintarMyGame/PintarMyGame/World.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace PintarMyGame
 {
-    internal class World
+    public class World
     {
+
     }
 }
