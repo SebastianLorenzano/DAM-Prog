14af9ee928efa02ec85a9969ef1240e7ff9d3fa3
SebastianLorenzano <selorenzano3@gmail.com>
2023-11-03 16:58:56 +0100

    03-11-23

--- Prog/EV1/Classes/Classes/ChessFigure.cs
+++ Prog/EV1/Classes/Classes/ChessFigure.cs
@@ -8,8 +8,13 @@ using System.Threading.Tasks;
 namespace Classes
 {
 
+
+
+
     public class ChessFigure
     {
+
+
         public enum FigureType
         {
             PAWN,
@@ -30,18 +35,33 @@ namespace Classes
         private ColorType _color;
         public FigureType _figure;
 
-        
-        public bool IsValid()
+        public ChessFigure(int x, int y, ColorType color, FigureType figure)
         {
-            return _x >= 1 && _x <= 8 && _y >= 1 && _y <= 8;
+            _x = x;
+            _y = y;
+            _color = color;
+            _figure = figure;
+            _movementCount = 0;
+        }
+
+        public bool IsValid(int x, int y)
+        {
+            return x >= 1 && x <= 8 && y >= 1 && y <= 8;
         }
 
         public int GetX()
-            { return _x; }
+        { 
+            if (IsValid(_x, _y))
+                return _x;
+            return -1;
+        }
         
         public int GetY()
-        { return _y; }
-
+        { 
+            if (IsValid(_x, _y))
+                return _y; 
+            return -1;
+        }
         public FigureType GetFigureType()
         {
             return _figure;
@@ -53,27 +73,55 @@ namespace Classes
      
         }
 
-        internal void MoveTo()
+        internal void MoveTo(int TargetX, int TargetY)
         {
-
+            if (IsValid(TargetX, TargetY))
+            {
+                _x = TargetX;
+                _y= TargetY;
+                _movementCount++;
+            }    
         }
 
         public void Promote()
         {
-
-        }
+            Console.Write("ESCRIBIR NOMBRE DE PIEZA A PROMOCIONAR EN MAYUSC: ");
+            var value = Console.ReadLine();
+            var bool1 = true;
+            while (bool1 == true)
+            {
+                if (value == "TORRE")
+                {
+                    _figure = FigureType.TOWER;
+                    return;
+                }
+                if (value == "CABALLERO")
+                {
+                    _figure = FigureType.KNIGHT;
+                    return;
+                }
+                if (value == "ALFIL")
+                {
+                    _figure = FigureType.BISHOP;
+                    return;
+                }
+                if (value == "REINA")
+                {
+                    _figure = FigureType.QUEEN;
+                    return;
+                }
+                Console.WriteLine("VALOR NO RECONOCIDO. INTENTALO DE VUELTA.");
+                }
+            }
+        
 
         public int GetMovementCount()
-        {
-
-        }
+        { return _movementCount; }
 
         public bool HasBeenMoved()
-        {
-            return
-        }
-
-    }
-
-
+        { return _movementCount > 0; }
 }
+}
+
+
+
