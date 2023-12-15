7d79de3a6d0562f5f3758e4a52781fdb89685211
SebastianLorenzano <selorenzano3@gmail.com>
2023-12-15 17:55:28 +0100

    added 3Herencias/basura15-12-23.sln

---
+++ 3Herencias/basura15-12-23/ConsoleApp1/IBlueprint.cs
@@ -0,0 +1,18 @@
+﻿
+namespace ConsoleApp1
+    
+{
+    public interface IBlueprint
+    {
+        public void AddShape(IShape shape);
+
+        public int GetShapeCount();
+
+        public IShape GetShapeAt(int index);
+
+        public void RemoveShapeAt(int index);
+
+        public double GetArea();
+
+    }
+}
---
+++ 3Herencias/basura15-12-23/ConsoleApp1/ShapeWithoutArea.cs
@@ -0,0 +1,8 @@
+﻿
+
+namespace ConsoleApp1
+{
+    public class ShapeWithoutArea
+    {
+    }
+}
