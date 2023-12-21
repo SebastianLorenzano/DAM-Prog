55495788d5a4c6371ad76dec88d96a6ba709f640
SebastianLorenzano <selorenzano3@gmail.com>
2023-12-21 17:12:46 +0100

    21-12-23

--- 3Herencias/basura15-12-23/ConsoleApp1/Rect2D.cs
+++ 3Herencias/basura15-12-23/ConsoleApp1/Rect2D.cs
@@ -1,8 +1,14 @@
 ﻿
 
+using System.Dynamic;
+
 namespace ConsoleApp1
 {
     public class Rect2D
     {
+        private Point2D _min = new();
+        private Point2D _max = new();
+
+        
     }
 }
