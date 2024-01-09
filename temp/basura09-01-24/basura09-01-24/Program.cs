a2ad01c89662ce374c5f3deaeb0b9298f70fff57
SebastianLorenzano <selorenzano3@gmail.com>
2024-01-09 18:50:12 +0100

    added basura09-01-24

---
+++ temp/basura09-01-24/basura09-01-24/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace basura09_01_24
+{
+    public class Caso1
+    {
+        private string _name;
+        
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+    }
+
+    public class Caso2
+    {
+        private string _name;
+        public string Name => _name;
+    }
+
+    public class Caso3 /* Se crea la variable pero no podes manejarla ni verla*/
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+    }
+}
