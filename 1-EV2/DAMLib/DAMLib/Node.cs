8ab8a266488fbe94512b5d42f5eae5e4b5151049
SebastianLorenzano <selorenzano3@gmail.com>
2024-02-08 15:56:46 +0100

    Update Node.cs

--- 1-EV2/DAMLib/DAMLib/Node.cs
+++ 1-EV2/DAMLib/DAMLib/Node.cs
@@ -6,26 +6,45 @@ namespace DAMLib
     public delegate bool CheckDelegate<T>(Node<T> node);
     public class Node<T>
     {
-        public T _item;
-        List<Node<T>> _children = new();
-        WeakReference<Node<T>?> _parent = new(null);
+        public T? _item;
+        List<Node<T>> _children;
+        WeakReference<Node<T>?> _parent = new(null);        //TODO: Make _parent work at the beggining as null and for it to create when needed
 
-        public Node<T>? Parent 
+        public Node<T>? Parent
         {
-            get 
+            get
             {
                 Node<T>? parent;
                 _parent.TryGetTarget(out parent);
-                return parent;                       
+                return parent;
             }
-            
+
             set => SetParent(value!);           //El signo de exclamacion lo que hace aqui es sacar la advertencia de null, 
         }                                           //es equivalente a poner una instruccion #nullable disable
         public bool IsRoot => Parent == null;
-        public bool IsLeaf => _children.Count == 0;
-        public int ChildrenCount => _children.Count;
+        public bool IsLeaf => _children == null || _children.Count == 0;
+        public int ChildrenCount
+        {
+            get
+            {
+                if (_children == null)
+                    return 0;
+                return _children.Count;
+            }
+        }
         public int Level => GetLevel();
         public Node<T> Root => GetRoot();
+
+
+        public Node()
+        {
+
+        }
+
+        public Node(T? item)
+        {
+            _item = item;
+        }
 
         public int GetLevel()
         { 
@@ -66,7 +85,7 @@ namespace DAMLib
 
         public Node<T>? GetChildAt(int index)
         {
-            return (index < 0 || index >= _children.Count) ? null : _children[index];
+            return (index < 0 || index >= ChildrenCount) ? null : _children[index];
         }
 
         public void Unlink()
@@ -83,12 +102,13 @@ namespace DAMLib
         {
             if (node == null)
                 return;
+            if (_children == null)
+                _children = new();
             node.Unlink();
             node._parent.SetTarget(this);
             if (_children == null)
                 _children = new();
             _children.Add(node);
-
         }
 
         public void SetParent(Node<T>? node)
@@ -98,7 +118,7 @@ namespace DAMLib
                 Unlink(); 
                 return;
             }
-            if (Contains(node))
+            if (_children != null && Contains(node))
                 throw new ArgumentException("Tried Adding its own child as a Parent.");
             node.AddChild(this);
         }
@@ -118,7 +138,8 @@ namespace DAMLib
                 return false;
             if (this == node) 
                 return true;
-            for (int i = 0; i< _children.Count; i++)
+            int n = ChildrenCount;
+            for (int i = 0; i < n; i++)
             {
                 var c = _children[i];
                 if (c == node || c.ContainsDescendant(node)) 
@@ -142,10 +163,10 @@ namespace DAMLib
                 return null;
             if (checker(this))
                 return this;
-            for (int i = 0; i < _children.Count; i++)
+            int n = ChildrenCount;
+            for (int i = 0; i < n; i++)
             {
-                var child = _children[i];
-                var result = child.FindNode(checker);
+                var result = _children[i].FindNode(checker);
                 if (result != null)
                     return result;
             }
@@ -165,8 +186,8 @@ namespace DAMLib
         {
             if (checker(this))
                 result.Add(this);
-
-            for (int i = 0; i < _children.Count; i++)
+            int n = ChildrenCount;
+            for (int i = 0; i < n; i++)
                 _children[i].FindNodes(checker, result);
         }
         
