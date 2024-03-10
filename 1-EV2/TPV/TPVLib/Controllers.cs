884e4c302ba16695137dfbe9d8c6b7d02bd53b3f
SebastianLorenzano <selorenzano3@gmail.com>
2024-03-10 16:26:31 +0100

    10-03-24

--- 1-EV2/TPV/TPVLib/Controllers.cs
+++ 1-EV2/TPV/TPVLib/Controllers.cs
@@ -3,51 +3,5 @@ namespace TPVLib
 {
     public class Controllers
     {
-
-        public static void ShowMainMenu(ITPV tpv)
-        {
-
-
-
-
-            Console.WriteLine("1- Products");
-            Console.WriteLine("2- Tickets");
-            Console.WriteLine("3- Summary");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("9- Save and Exit");
-
-        }
-
-        public static void ShowProductsMenu(ITPV tpv)
-        {
-            Console.WriteLine("1-Add Product");
-            Console.WriteLine("2-Modify Product");
-            Console.WriteLine("3-Delete Product");
-            Console.WriteLine("");
-            Console.WriteLine("5- Go Back to Main Menu");
-            Console.WriteLine("6- Go to Tickets");
-            Console.WriteLine("7- Go to Summary");
-            Console.WriteLine("");
-            Console.WriteLine("9- Save and Exit");
-        }
-
-        public static void ShowTicketsMenu(ITPV tpv)
-        {
-            Console.WriteLine("1- Products");
-            Console.WriteLine("1-Add Ticket");
-            Console.WriteLine("2-Change Ticket");
-            Console.WriteLine("3-Delete Ticket");
-            Console.WriteLine("");
-            Console.WriteLine("5- Go to Products");
-            Console.WriteLine("6- Go Back to Main Menu");
-            Console.WriteLine("7- Summary");
-            Console.WriteLine("");
-            Console.WriteLine("9- Save and Exit");
-        }
-
     }
 }
