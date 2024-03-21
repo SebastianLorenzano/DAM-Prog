62433118dfc9aa85b9b7be6a9e8f8429af0b4105
SebastianLorenzano <selorenzano3@gmail.com>
2024-03-21 17:02:28 +0100

    created basura21-03-24

---
+++ 2-TESTING/basura21-03-24/basura21-03-24/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace basura21_03_24
+{
+    internal class Program
+    {
+        public class Prueba : IDisposable
+        {
+            public void Dispose()
+            {
+                Console.WriteLine("Dispose");
+            }
+
+            public void SayHello()
+            {
+                Console.WriteLine("Hello");
+            }
+        }
+
+        public static Prueba CrearPrueba()
+        {
+            return null;
+        }
+
+        static void Main(string[] args)
+        {
+            using (Prueba p = CrearPrueba())
+            {
+                //p.SayHello();
+            };
+
+        }
+    }
+}
