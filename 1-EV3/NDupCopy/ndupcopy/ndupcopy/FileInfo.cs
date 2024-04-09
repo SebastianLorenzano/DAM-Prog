3c761e238d6322964a64e456594519d98ab0571c
SebastianLorenzano <selorenzano3@gmail.com>
2024-04-09 16:18:42 +0200

    Create FileInfo.cs

---
+++ 1-EV3/NDupCopy/ndupcopy/ndupcopy/FileInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices;
+
+namespace ndupcopy
+{
+    public class FileInfo
+    {
+        public int Hash { get; set; }
+        public double Size { get; set; }
+        public string? Path { get; set; }
+        public bool IsDisabled { get; set; }
+
+        private FileInfo()
+        {
+
+        }
+
+        public static FileInfo? Create(int hash, double size, string path, bool is_disabled = false)
+        {
+            if (path != null)
+                return new FileInfo()
+                { Hash = hash, Size = size, Path = path, IsDisabled = is_disabled};
+            return null;
+        }
+    }
+}
