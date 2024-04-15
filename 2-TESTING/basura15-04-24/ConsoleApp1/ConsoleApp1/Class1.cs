b026e930e9d3a679ff1cf2d92ac6ae3e5bb13e8f
SebastianLorenzano <selorenzano3@gmail.com>
2024-04-15 15:53:01 +0200

    15-04-24

---
+++ 2-TESTING/basura15-04-24/ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Class1
+    {
+    }
+
+
+    public partial class Pepe
+    {
+        public string DNI { get; set; }
+
+        public Pepe(string dni)
+        {
+            if (dni != null)
+                DNI = dni;
+        }
+    }
+
+
+    public partial class Pepe
+    {
+        public void PrintDNI()
+        {
+            Console.WriteLine(DNI);
+        }
+    }
+
+}
---
+++ 2-TESTING/basura15-04-24/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Pepe("4321124");
+            Console.WriteLine("El DNI de Pepe es: " + p.DNI);
+        }
+    }
+
+
+}
