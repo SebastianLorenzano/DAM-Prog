6b3693962d1263e73d618d565ec494ad4e24d7ec
SebastianLorenzano <selorenzano3@gmail.com>
2024-04-16 17:09:11 +0200

    16-04-24

---
+++ 2-TESTING/basuraEDE16-04-24/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, count = 0;
+            bool isNumber;
+            Console.WriteLine("Escribe un número.");
+            string answer = Console.ReadLine();
+            isNumber = int.TryParse(answer, out num);
+            if (!isNumber)
+            {
+                Console.WriteLine("Tienes que escribir un número.");
+                Main(Array.Empty<string>());
+                return;
+            }
+
+            for (int i = 1; i < num; i++)
+                if (num % i == 0)
+                {
+                    count++;
+                    if (count > 2)
+                        break;
+                }
+                    
+            if (count <= 2)
+                Console.WriteLine("El número " + num + " es primo.");
+            else
+                Console.WriteLine("El número " + num + " no es primo.");
+
+        }
+    }
+}
+
+
