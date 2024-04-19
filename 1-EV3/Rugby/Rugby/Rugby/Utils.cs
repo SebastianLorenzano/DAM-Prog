485c1efa0b73932eb8d5a4c6706069d821adaf06
SebastianLorenzano <selorenzano3@gmail.com>
2024-04-19 17:15:27 +0200

    Update Utils.cs

--- 1-EV3/Rugby/Rugby/Rugby/Utils.cs
+++ 1-EV3/Rugby/Rugby/Rugby/Utils.cs
@@ -16,7 +16,37 @@
             return (int)(Math.Floor(Math.Sqrt(dx * dx + dy * dy)));       // Rounds down the distance to the nearest integer and casts it
         }
 
-        public 
+        public static List<Player> GetTeammates(Player player, Stadium stadium)
+        {
+            List<Player> result = new();
+            for (int i = 0; i < stadium.CharCount; i++)
+            {
+                Character? character = stadium.GetCharacterAtIndex(i);
+                if (character is Player)
+                {
+                    Player p = (Player)character;
+                    if (p.Team == player.Team && p != player)
+                        result.Add(p);
+                }
+            }
+            return result;
+        }
+
+        public static List<Player> GetTeammatesOnPointLine(Player player, Stadium stadium)
+        {
+            var result = GetTeammates(player, stadium);
+            int y;
+            if (player.Team == stadium.FirstTeam)
+                y = 0;
+            else
+                y = 9;
+            foreach (var p in result)
+            {
+                if (Math.Abs( p.y - y) <= 2)
+                    result.Add(p);
+            }
+            return result;
+        }
 
     }
 }
