b513e4236f31328a5d28de0f32096642e1de8bf5
SebastianLorenzano <selorenzano3@gmail.com>
2024-04-23 17:41:45 +0200

    updated EDE UD6.3

--- 1-EV3/NDupCopy/ndupcopy/ndupcopy/FileMover.cs
+++ 1-EV3/NDupCopy/ndupcopy/ndupcopy/FileMover.cs
@@ -2,10 +2,10 @@
 {
     public class FileMover
     {
-        public static void CopyFile(string originFolder, string absolutePath, string destination)
+        public static string? CopyFile(string originFolder, string absolutePath, string destination)
         {
             if (absolutePath == null || destination == null)
-                return;
+                return null;
             if (!Directory.Exists(destination))
                 throw new Exception("Directory does not exist or file does not exist");
             if (!File.Exists(absolutePath))
@@ -16,7 +16,8 @@
 
             if (!Directory.Exists(Path.GetDirectoryName(trueDestination)))
                 Directory.CreateDirectory(Path.GetDirectoryName(trueDestination));
-            File.Copy(absolutePath, destination);
+            File.Copy(absolutePath, trueDestination);
+            return trueDestination;
         }
     }
 }
---
+++ 5-EDE/UD6/UD6.3/ED - Práctica 3 - Base/ConsoleApp1/ConsoleApp1/Alumnos.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Alumno
+    {
+        private string nombre;
+        private int nota;
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+        public int Nota
+        {
+            get { return nota; }
+            set
+            {
+                if (value >= 0 && value <= 10)
+                    nota = value;
+            }
+        }
+        public Boolean Aprobado
+        {
+            get
+            {
+                if (nota >= 5)
+                    return true;
+                else
+                    return false;
+            }
+        }
+    }
+
+    public class Alumnos
+    {
+        private ArrayList listaAlumnos = new ArrayList();
+
+        // Agrega un nuevo alumno a la lista
+        //        
+        public void Agregar(Alumno alu)
+        {
+            listaAlumnos.Add(alu);
+        }
+
+        // Devuelve el alumno que está en la posición num
+        //
+        public Alumno Obtener(int num)
+        {
+            //modificación código
+            if (listaAlumnos.Count == 0)
+            {
+                return null;
+            }
+            else //hasta aquí
+            if (num >= 0 && num < listaAlumnos.Count) // modificación código, se modifico num <= listaAlumnos.Count por num < listaAlumnos.Count
+            { 
+                return ((Alumno)listaAlumnos[num]);
+            }
+            return null;
+        }
+
+        // Devuelve la nota media de los alumnos
+        //
+        public float Media
+        {
+            get
+            {
+                if (listaAlumnos.Count == 0)
+                    return 0;
+                else
+                {
+                    float media = 0;
+                    for (int i = 0; i < listaAlumnos.Count; i++)
+                    {
+                        media += ((Alumno)listaAlumnos[i]).Nota;
+                    }
+                    return (media / listaAlumnos.Count);
+                }
+            }
+        }
+    }
+}
