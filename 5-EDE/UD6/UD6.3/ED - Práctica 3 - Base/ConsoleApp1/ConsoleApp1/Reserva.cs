54467e72e59cfb86bd6994ea132345d6db5b8544
SebastianLorenzano <selorenzano3@gmail.com>
2024-05-10 22:50:10 +0200

    10-05-24

---
+++ 5-EDE/UD6/UD6.3/ED - Práctica 3 - Base/ConsoleApp1/ConsoleApp1/Reserva.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Reserva
+    {
+        private string nif = String.Empty;
+        private int numPlazas;
+
+        public string Nif
+        {
+            get 
+            { 
+                return nif; 
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentException("El NIF no puede estar vacío");
+                nif = value;
+            }
+        }
+
+        public int NumPlazas
+        {
+            get { return numPlazas; }
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentException("El número de plazas debe ser mayor que cero");
+                numPlazas = value;
+            }
+        }
+    }
+}
