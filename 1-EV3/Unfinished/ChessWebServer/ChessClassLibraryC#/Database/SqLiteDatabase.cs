44b0a84480ae6af59bb9efd2987e2c43aad99a57
SebastianLorenzano <selorenzano3@gmail.com>
2024-05-21 13:37:18 +0200

    21-05-24

---
+++ 1-EV3/Unfinished/ChessWebServer/ChessClassLibraryC#/Database/SqLiteDatabase.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Database
+{
+    public class SqLiteDatabase : IDatabase
+    {
+        private static SqLiteDatabase _database = new();
+        public static SqLiteDatabase Instance => _database; // How can I prevent for people to put it to null?
+
+        public long AddGame(Game game)
+        {
+            throw new NotImplementedException();
+        }
+
+        public long AddMovement(Movement mov)
+        {
+            throw new NotImplementedException();
+        }
+
+        public long AddUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Game? GetGameWithId(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Movement? GetMovementWithId(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User? GetUserWithId(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveGame(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveMovement(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveUser(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateGame(long id, Game game)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateMovement(long id, Movement mov)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateUser(long id, User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- 1-EV3/Unfinished/ChessWebServer/ChessClassLibraryC#/Database/User.cs
+++ 1-EV3/Unfinished/ChessWebServer/ChessClassLibraryC#/Database/User.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Database
 {
-    internal class User
+    public class User
     {
         public int Id { get; set; }
         public string Name { get; set; }
