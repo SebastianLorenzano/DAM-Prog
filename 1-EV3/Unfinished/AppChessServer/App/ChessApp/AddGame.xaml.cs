e7f2e4b8fc0a1c5ede253f0cef98c388b5bbd8e6
SebastianLorenzano <selorenzano3@gmail.com>
2024-06-02 16:09:40 +0200

    02-06-24

---
+++ 1-EV3/Unfinished/AppChessServer/App/ChessApp/AddGame.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChessApp
+{
+    /// <summary>
+    /// Lógica de interacción para AddGame.xaml
+    /// </summary>
+    public partial class AddGame : Page
+    {
+        public AddGame()
+        {
+            InitializeComponent();
+        }
+    }
+}
